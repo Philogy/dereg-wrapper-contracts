217187ddbc868e89f07dcc3b155832b7f8889437
philogy <philippe@dumo.net>
2022-10-08 18:48:24 -0500

    Add freeze and settlement methods

--- src/AssetLayerV0_1.sol
+++ src/AssetLayerV0_1.sol
@@ -3,15 +3,18 @@ pragma solidity 0.8.15;
 
 import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
 import {SafeCastLib} from "solmate/utils/SafeCastLib.sol";
-import {Math} from "@openzeppelin/utils/math/Math.sol";
 import {ERC20} from "solmate/tokens/ERC20.sol";
+import {MerkleProofLib} from "solady/utils/MerkleProofLib.sol";
+import {Math} from "@openzeppelin/utils/math/Math.sol";
 import {IERC721} from "@openzeppelin/token/ERC721/IERC721.sol";
 import {LogicProxy} from "./utils/LogicProxy.sol";
+import {IAssetLayerV0_1} from "./IAssetLayerV0_1.sol";
 
 /// @author philogy <https://github.com/philogy>
-contract AssetLayer {
+contract AssetLayer is IAssetLayerV0_1 {
     using SafeTransferLib for ERC20;
     using SafeCastLib for uint256;
+    using MerkleProofLib for bytes32[];
 
     event WithdrawalAdded(
         WithdrawalType indexed wtype,
@@ -26,8 +29,11 @@ contract AssetLayer {
     error TooLargeTotalDelay();
     error AlreadySettled();
     error NotYetSettled();
+    error UnknownSettlementStatus();
     error NonexistentWithdrawal();
     error InvalidWithdrawalType();
+    error Frozen();
+    error NotFrozen();
 
     enum WithdrawalType {
         None,
@@ -44,20 +50,31 @@ contract AssetLayer {
         uint256 assetDenominator; // amount token ID
     }
 
-    uint8 internal constant NO_ENTRY = 1;
-    uint8 internal constant SINGLE_ENTRY = 2;
+    struct Freeze {
+        uint64 lastEnd;
+        uint64 start;
+        bytes32 validWithdrawalsRoot;
+    }
 
     address public immutable factory;
 
     mapping(uint256 => Withdrawal) public getWithdrawal;
+    mapping(uint256 => Freeze) public getFreeze;
 
+    uint8 internal constant NO_ENTRY = 1;
+    uint8 internal constant SINGLE_ENTRY = 2;
+
+    uint8 internal reentryGuard = NO_ENTRY;
     uint64 internal nextWithdrawalId;
     uint24 public withdrawDefaultDelay;
-    uint8 internal reentryGuard = NO_ENTRY;
     address payable public logicModule;
+
     uint64 internal delayExtendTime = 1;
     uint24 internal delayExtension = 1;
-    uint64 internal frozenAt;
+    uint64 internal lastFreeze;
+    uint64 internal nextFreezeId;
+    bool internal isFrozen;
+
     address public upgrader;
 
     modifier only(address _authAccount) {
@@ -80,10 +97,15 @@ contract AssetLayer {
         _;
     }
 
-    constructor(address payable _logicModule, address _upgrader) {
+    modifier whileNotFrozen() {
+        if (isFrozen) revert Frozen();
+        _;
+    }
+
+    constructor(address _upgrader, address _initialLogicImpl) {
         factory = msg.sender;
-        logicModule = _logicModule;
         upgrader = _upgrader;
+        logicModule = payable(new LogicProxy(address(this), _initialLogicImpl));
     }
 
     receive() external payable {}
@@ -224,45 +246,78 @@ contract AssetLayer {
             _addWithdrawal(WithdrawalType.ERC721, _token, _recipient, _tokenId);
     }
 
-    function executeSettlement(uint256 _withdrawalId)
+    function executeDirectSettlement(uint256 _withdrawalId)
         external
         existingWithdrawal(_withdrawalId)
     {
         if (!settled(_withdrawalId)) revert NotYetSettled();
-        WithdrawalType wtype = getWithdrawal[_withdrawalId].wtype;
-        address asset = getWithdrawal[_withdrawalId].asset;
-        address recipient = getWithdrawal[_withdrawalId].recipient;
-        uint256 assetDenominator = getWithdrawal[_withdrawalId]
-            .assetDenominator;
-        delete getWithdrawal[_withdrawalId];
-        if (wtype == WithdrawalType.ERC20) {
-            ERC20(asset).safeTransfer(recipient, assetDenominator);
-        } else if (wtype == WithdrawalType.ERC721) {
-            IERC721(asset).transferFrom(
-                address(this),
-                recipient,
-                assetDenominator
-            );
-        } else {
-            revert InvalidWithdrawalType();
-        }
+        _executeSettlement(_withdrawalId);
+    }
+
+    function executePreFreezeSettlement(
+        uint256 _withdrawalId,
+        uint256 _freezeId
+    ) external existingWithdrawal(_withdrawalId) {
+        (uint256 origTime, uint256 settlementTime) = _getSettlementTime(
+            _withdrawalId
+        );
+        uint256 lastEnd = getFreeze[_freezeId].lastEnd;
+        uint256 start = getFreeze[_freezeId].start;
+        if (origTime < lastEnd || start < settlementTime)
+            revert UnknownSettlementStatus();
+        _executeSettlement(_withdrawalId);
+    }
+
+    function executeValidWithdrawalSettlement(
+        uint256 _withdrawalId,
+        uint256 _freezeId,
+        bytes32[] calldata _proof
+    ) external existingWithdrawal(_withdrawalId) {
+        if (
+            !_proof.verify(
+                getFreeze[_freezeId].validWithdrawalsRoot,
+                bytes32(_withdrawalId)
+            )
+        ) revert UnknownSettlementStatus();
+        _executeSettlement(_withdrawalId);
     }
 
     function settled(uint256 _withdrawalId) public view returns (bool) {
-        return _getSettlementTime(_withdrawalId) <= block.timestamp;
+        (uint256 origTime, uint256 settlementTime) = _getSettlementTime(
+            _withdrawalId
+        );
+        if (origTime < lastFreeze) revert UnknownSettlementStatus();
+        return settlementTime <= block.timestamp;
     }
 
     /*//////////////////////////////////////////////////////////////
                        EMERGENCY ACTIONS
     //////////////////////////////////////////////////////////////*/
 
-    function extendWithdrawal(uint256 _withdrawalId, uint24 _addedDelay)
+    function extendWithdrawalDelay(uint256 _withdrawalId, uint24 _addedDelay)
         external
         only(factory)
         existingWithdrawal(_withdrawalId)
     {
         if (settled(_withdrawalId)) revert AlreadySettled();
         getWithdrawal[_withdrawalId].delay += _addedDelay;
+    }
+
+    function freeze() external only(factory) whileNotFrozen {
+        lastFreeze = uint64(block.timestamp);
+        uint256 freezeId = nextFreezeId;
+        isFrozen = true;
+        getFreeze[freezeId].start = uint64(block.timestamp);
+    }
+
+    function unfreeze(bytes32 _validWithdrawalsRoot)
+        external
+        only(factory)
+        whileNotFrozen
+    {
+        if (!isFrozen) revert NotFrozen();
+        isFrozen = false;
+        getFreeze[nextFreezeId++].validWithdrawalsRoot = _validWithdrawalsRoot;
     }
 
     /*//////////////////////////////////////////////////////////////
@@ -274,7 +329,7 @@ contract AssetLayer {
         address _asset,
         address _recipient,
         uint256 _assetDenominator
-    ) internal returns (uint256 withdrawalId) {
+    ) internal whileNotFrozen returns (uint256 withdrawalId) {
         unchecked {
             withdrawalId = nextWithdrawalId++;
             uint256 delay = withdrawDefaultDelay;
@@ -297,14 +352,38 @@ contract AssetLayer {
         }
     }
 
+    function _executeSettlement(uint256 _withdrawalId) internal {
+        WithdrawalType wtype = getWithdrawal[_withdrawalId].wtype;
+        address asset = getWithdrawal[_withdrawalId].asset;
+        address recipient = getWithdrawal[_withdrawalId].recipient;
+        uint256 assetDenominator = getWithdrawal[_withdrawalId]
+            .assetDenominator;
+        delete getWithdrawal[_withdrawalId];
+        if (wtype == WithdrawalType.ERC20) {
+            ERC20(asset).safeTransfer(recipient, assetDenominator);
+        } else if (wtype == WithdrawalType.ERC721) {
+            IERC721(asset).transferFrom(
+                address(this),
+                recipient,
+                assetDenominator
+            );
+        } else {
+            revert InvalidWithdrawalType();
+        }
+    }
+
     function _getSettlementTime(uint256 _withdrawalId)
         internal
         view
-        returns (uint256)
+        returns (uint256 origTime, uint256 settlementTime)
     {
-        uint256 origTime = getWithdrawal[_withdrawalId].enqueuedAt;
+        origTime = getWithdrawal[_withdrawalId].enqueuedAt;
         uint256 origDelay = getWithdrawal[_withdrawalId].enqueuedAt;
-        return _addDelay(origTime + origDelay, delayExtendTime, delayExtension);
+        settlementTime = _addDelay(
+            origTime + origDelay,
+            delayExtendTime,
+            delayExtension
+        );
     }
 
     function _addDelay(
